f02e17ebac59e57a27db6683991296488e0c2d56
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 04:53:37 +0100

    Replace temp with query – totalVolumeCredits

--- example-1/statement.mjs
+++ example-1/statement.mjs
@@ -1,6 +1,5 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -10,12 +9,17 @@ export function statement(invoice, plays) {
     )} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf, playsFor(perf));
   }
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
