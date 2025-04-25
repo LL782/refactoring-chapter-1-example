1a6bdbfca793c7ebf6f34f33f499b7c718e370c0
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 05:01:56 +0100

    Rename function

--- example-1/statement.mjs
+++ example-1/statement.mjs
@@ -3,8 +3,8 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     // print line for this order
-    result += `  ${playsFor(perf).name}: ${usd(
-      amountFor(perf, playsFor(perf))
+    result += `  ${playFor(perf).name}: ${usd(
+      amountFor(perf, playFor(perf))
     )} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -14,7 +14,7 @@ export function statement(invoice, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of invoice.performances) {
-      result += amountFor(perf, playsFor(perf));
+      result += amountFor(perf, playFor(perf));
     }
     return result;
   }
@@ -35,21 +35,21 @@ export function statement(invoice, plays) {
     }).format(aNumber / 100);
   }
 
-  function playsFor(aPerformance) {
+  function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playsFor(aPerformance).type)
+    if ("comedy" === playFor(aPerformance).type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playsFor(aPerformance).type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -64,7 +64,7 @@ export function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playsFor(aPerformance).type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
