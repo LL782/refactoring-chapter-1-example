fb99823401c2e0ac0f70b8d47bface7ec8a2840f
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 08:47:37 +0100

    Apply the "Replace loop with pipeline" refactoring to totalAmount and totalVolumeCredits

--- example-1/createStatementData.mjs
+++ example-1/createStatementData.mjs
@@ -58,18 +58,13 @@ export function createStatementData(invoice, plays) {
   }
 
   function totalAmount() {
-    let result = 0;
-    for (let perf of statementData.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return statementData.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of statementData.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return statementData.performances.reduce(
+      (total, p) => total + p.volumeCredits,
+      0
+    );
   }
 }
