22be485c5eed194dda074e0906d230a0464c48c6
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 05:43:06 +0100

    Split phase 3: Enrich intermediate data with play

--- example-1/statement.mjs
+++ example-1/statement.mjs
@@ -1,18 +1,28 @@
 export function statement(invoice, plays) {
   const data = {};
   data.customer = invoice.customer;
-  data.performances = invoice.performances;
+  data.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(data, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = { ...aPerformance };
+    result.play = playFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {
     // print line for this order
-    result += `  ${playFor(perf).name}: ${usd(
-      amountFor(perf, playFor(perf))
-    )} (${perf.audience} seats)\n`;
+    result += `  ${perf.play.name}: ${usd(amountFor(perf, perf.play))} (${
+      perf.audience
+    } seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -21,7 +31,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf, playFor(perf));
+      result += amountFor(perf, perf.play);
     }
     return result;
   }
@@ -42,21 +52,17 @@ function renderPlainText(data, plays) {
     }).format(aNumber / 100);
   }
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -71,7 +77,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
     }
     return result;
   }
