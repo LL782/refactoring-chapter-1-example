b759594222a38c2e6a75d8420b85317ad3becdde
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 08:02:49 +0100

    Add htmlStatement function

--- example-1/statement.test.js
+++ example-1/statement.test.js
@@ -1,32 +1,32 @@
 import { expect, test, describe } from "vitest";
-import { statement } from "./statement.mjs";
+import { htmlStatement, statement } from "./statement.mjs";
+
+const invoice = {
+  customer: "BigCo",
+  performances: [
+    {
+      playID: "asYouLikeIt",
+      audience: 35,
+    },
+    {
+      playID: "othello",
+      audience: 40,
+    },
+    {
+      playID: "hamlet",
+      audience: 55,
+    },
+  ],
+};
+
+const plays = {
+  asYouLikeIt: { name: "As You Like It", type: "comedy" },
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
 
 describe("statement", () => {
   test("generates a statement based on performance data", () => {
-    const invoice = {
-      customer: "BigCo",
-      performances: [
-        {
-          playID: "asYouLikeIt",
-          audience: 35,
-        },
-        {
-          playID: "othello",
-          audience: 40,
-        },
-        {
-          playID: "hamlet",
-          audience: 55,
-        },
-      ],
-    };
-
-    const plays = {
-      asYouLikeIt: { name: "As You Like It", type: "comedy" },
-      hamlet: { name: "Hamlet", type: "tragedy" },
-      othello: { name: "Othello", type: "tragedy" },
-    };
-
     expect(statement(invoice, plays)).toMatchInlineSnapshot(`
       "Statement for BigCo
         As You Like It: $580.00 (35 seats)
@@ -34,6 +34,20 @@ describe("statement", () => {
         Hamlet: $650.00 (55 seats)
       Amount owed is $1,730.00
       You earned 47 credits
+      "
+    `);
+  });
+
+  test("generates an HTML statement based on performance data", () => {
+    expect(htmlStatement(invoice, plays)).toMatchInlineSnapshot(`
+      "<h1>Statement for BigCo</h1>
+      <table>
+      <tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>
+        <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>
+        <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>
+      </table>
+      <p>Amount owed is <em>$1,730.00</em></p>
+      <p>You earned <em>47</em> credits</p>
       "
     `);
   });
