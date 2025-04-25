9e14941ef3a1d5caf1bb68e24029fbb57efa7f9d
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 04:28:52 +0100

    Extract playsFor function

--- example-1/statement.mjs
+++ example-1/statement.mjs
@@ -9,7 +9,7 @@ export function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playsFor(perf);
     const thisAmount = amountFor(perf, play);
 
     // add volume credits
@@ -26,6 +26,10 @@ export function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function playsFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   function amountFor(aPerformance, play) {
     let result = 0;
