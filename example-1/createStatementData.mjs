c5bb5bfa239105e61bedfd6f2c3fcb889b9ce13e
Laurence Lord <laurence.lord@ledger.fr>
2025-04-25 08:59:12 +0100

    Add subclasses TragedyCalculator and ComedyCalculator

--- example-1/createStatementData.mjs
+++ example-1/createStatementData.mjs
@@ -3,38 +3,50 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = play;
   }
+
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error("subclass responsibility");
   }
+
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
     return result;
   }
 }
 
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
 function createPerformanceCalculator(aPerformance, play) {
-  return new PerformanceCalculator(aPerformance, play);
+  switch (play.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, play);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, play);
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
 }
 
 export function createStatementData(invoice, plays) {
